1036277dbdd025757ef2e06eee6ff4be1e26507e
1noseA <1noseA2L@gmail.com>
2024-02-03 19:25:16 +0900

    first commit

---
+++ src/test/java/com/manualapi/ManualApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manualapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManualApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
