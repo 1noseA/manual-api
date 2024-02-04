48f43713b28544f9bead67aabecb0015231bfa99
1noseA <1noseA2L@gmail.com>
2024-02-05 00:18:42 +0900

    SQL単純ミス

--- src/main/java/com/manualapi/ManualsMapper.java
+++ src/main/java/com/manualapi/ManualsMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
@@ -17,17 +18,18 @@ public interface ManualsMapper {
 	@Select("SELECT * FROM manuals WHERE manual_id = #{manualId}")
     public Manuals getDetail(int manualId);
 
+	@Options(useGeneratedKeys = true, keyProperty = "manualId")
 	@Insert("INSERT INTO manuals (user_id, display_order, title, start_date, end_date, content, link, created_by, created_at)"
 			+ "VALUES (#{userId}, #{displayOrder}, #{title}, #{startDate}, #{endDate}, #{content}, #{link}, #{createdBy}, CURRENT_TIMESTAMP)")
-    public boolean create(Manuals manual);
+    public void create(Manuals manual);
 
-    @Update("UPDATE manuals SET"
-    		+ "user_id =#{userId}, display_order = #{displayOrder}, title = #{title}, start_date = #{startDate}, end_date = #{endDate},"
-    		+ "content = #{content}, link = #{link}, updated_by = #{updatedBy}, updated_at = CURRENT_TIMESTAMP"
+    @Update("UPDATE manuals SET "
+    		+ "user_id = #{userId}, display_order = #{displayOrder}, title = #{title}, start_date = #{startDate}, end_date = #{endDate},"
+    		+ "content = #{content}, link = #{link}, updated_by = #{updatedBy}, updated_at = CURRENT_TIMESTAMP "
     		+ "WHERE manual_id = #{manualId}")
-    public boolean update(Manuals manual);
+    public void update(Manuals manual);
 
     @Delete("DELETE FROM manuals WHERE manual_id = #{manualId}")
-    public boolean delete(int manualId);
+    public void delete(int manualId);
 
 }
